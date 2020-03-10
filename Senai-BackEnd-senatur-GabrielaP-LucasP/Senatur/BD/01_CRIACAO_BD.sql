415abdd5a98b426cf38279b3b03c66c778053ae7
LucasPandolfi <54955146+LucasPandolfi@users.noreply.github.com>
2020-03-10 17:15:38 -0300

    Add files via upload

---
+++ Senai-BackEnd-senatur-GabrielaP-LucasP/Senatur/BD/01_CRIACAO_BD.sql
@@ -0,0 +1,24 @@
+CREATE DATABASE Senatur_Tarde_;
+USE Senatur_Tarde_;
+
+CREATE TABLE Pacotes (
+	IdPacote INT PRIMARY KEY IDENTITY,
+	NomePacote VARCHAR (255) not null,
+	Descricao VARCHAR (500) not null ,
+	DataIda DATE not null,
+	DataVolta DATE not null,
+	Valor DECIMAL (18,2) not null,
+	NomeCidade VARCHAR(255) not null
+);
+CREATE TABLE Usuarios (
+	IdUsuario INT PRIMARY KEY IDENTITY,
+	Email VARCHAR (255) not null,
+	Senha VARCHAR (255) UNIQUE NOT NULL,
+	IdTipoUsuario  INT FOREIGN KEY REFERENCES TiposUsuario (IdTipoUsuario)
+);
+CREATE TABLE TiposUsuario (
+	IdTipoUsuario INT PRIMARY KEY IDENTITY,
+	Titulo VARCHAR (255) not null
+);
+
+
